ee255cf71c7103dba68baafe29877f9a857d5132
atalayyoluc <atalayyoluc@gmail.com>
2023-01-24 13:42:26 +0300

    User Entity oluşturuldu

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User:AuditableEntity
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
