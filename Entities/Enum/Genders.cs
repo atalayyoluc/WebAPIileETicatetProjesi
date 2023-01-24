69b134b746d49e02035dc122c517d9f769acc4df
atalayyoluc <atalayyoluc@gmail.com>
2023-01-24 13:43:32 +0300

    Cinsiyet Enumu oluşturuldu

---
+++ Entities/Enum/Genders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Enum
+{
+    public enum Genders
+    {
+        Famele=0,
+        Male=1,
+    }
+}
