f2547dd08af3772c1b51fc4d4e82c644025f9f7c
atalayyoluc <atalayyoluc@gmail.com>
2023-01-24 15:12:16 +0300

    Web api katmanının program cs sınıfında düzenlemeler yapıldı.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,13 @@
+using DataAccess.Concrete.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("Server=.;Database=WebAPIETicaretDB;Trusted_Connection=True;",option=>option.MigrationsAssembly("DataAccess").MigrationsHistoryTable(HistoryRepository.DefaultTableName,"dbo")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
