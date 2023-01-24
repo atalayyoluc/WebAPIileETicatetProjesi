437f4a635c9991915c5fcb69c4c972be72ede8df
atalayyoluc <atalayyoluc@gmail.com>
2023-01-24 13:43:12 +0300

    User Dtoları oluşturuldu

---
+++ Entities/DTOS/UserDTOs/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOS.UserDTOs
+{
+    public class UserDto:IDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
