6ce82d20de03003ab413fcfe84aef1204afca6c7
atalayyoluc <atalayyoluc@gmail.com>
2023-01-24 13:42:07 +0300

    Entities katmanında base entity sınıfları oluşturuldu

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdateEntity
+    {
+        public int CreatedUserId { get ; set ; }
+        public DateTime CreatedDate { get; set; }
+        public int? UpdateUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+
+
+
+
+}
---
+++ Entities/Concrete/BaseEntities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
