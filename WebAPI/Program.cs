e5a3a3fc1c541e02feb755987c8cc4f4e782b0af
atalayyoluc <atalayyoluc@gmail.com>
2023-01-25 13:02:56 +0300

    program cs dosyası güncellendi. IUserService çağırıldığında Userservice erişmesi sağlandı

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using Busines.Abstract;
+using Busines.Concrete;
+using DataAccess.Abstract;
 using DataAccess.Concrete.Contexts;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 
@@ -7,7 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("Server=.;Database=WebAPIETicaretDB;Trusted_Connection=True;",option=>option.MigrationsAssembly("DataAccess").MigrationsHistoryTable(HistoryRepository.DefaultTableName,"dbo")));
+builder.Services.AddTransient<IUserDal,EfUserDal>();
+builder.Services.AddTransient<IUserService,UserService>();
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("Server=.;Database=WebAPIETicaretDB;Trusted_Connection=True;",option=>option.MigrationsAssembly("DataAccess").MigrationsHistoryTable(HistoryRepository.DefaultTableName,"dbo")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
