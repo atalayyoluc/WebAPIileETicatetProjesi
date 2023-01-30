0219f98a4ef84b7a7312330fc74f8acec259a6b6
atalayyoluc <atalayyoluc@gmail.com>
2023-01-30 13:15:44 +0300

    Revert "Windows form uygulaması ile Crud işlemleri yapıldı"
    This reverts commit 877381d0727abeaee861f520ecfa7eb0fa73a4d4.

--- WebAPIileFormUygulaması/Form1.Designer.cs
+++ WebAPIileFormUygulaması/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,264 +23,18 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
-            this.txtUserFirstName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtUserLastName = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtUserEmail = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtUserPassword = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtUserAdress = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = " Kullanıcı Adı";
-            // 
-            // txtUserName
-            // 
-            this.txtUserName.Location = new System.Drawing.Point(100, 35);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(297, 27);
-            this.txtUserName.TabIndex = 1;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(240, 262);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(94, 29);
-            this.btnAdd.TabIndex = 2;
-            this.btnAdd.Text = "Ekle";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.ImeMode = System.Windows.Forms.ImeMode.Disable;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 366);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(1159, 226);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
-            // 
-            // dtpDateOfBirth
-            // 
-            this.dtpDateOfBirth.Location = new System.Drawing.Point(523, 37);
-            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
-            this.dtpDateOfBirth.Size = new System.Drawing.Size(297, 27);
-            this.dtpDateOfBirth.TabIndex = 5;
-            // 
-            // txtUserFirstName
-            // 
-            this.txtUserFirstName.Location = new System.Drawing.Point(100, 74);
-            this.txtUserFirstName.Name = "txtUserFirstName";
-            this.txtUserFirstName.Size = new System.Drawing.Size(297, 27);
-            this.txtUserFirstName.TabIndex = 7;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 20);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Adı";
-            // 
-            // txtUserLastName
-            // 
-            this.txtUserLastName.Location = new System.Drawing.Point(100, 117);
-            this.txtUserLastName.Name = "txtUserLastName";
-            this.txtUserLastName.Size = new System.Drawing.Size(297, 27);
-            this.txtUserLastName.TabIndex = 9;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 124);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 20);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Soyadı";
-            // 
-            // txtUserEmail
-            // 
-            this.txtUserEmail.Location = new System.Drawing.Point(523, 117);
-            this.txtUserEmail.Name = "txtUserEmail";
-            this.txtUserEmail.Size = new System.Drawing.Size(297, 27);
-            this.txtUserEmail.TabIndex = 11;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 161);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 20);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Şifre";
-            // 
-            // txtUserPassword
-            // 
-            this.txtUserPassword.Location = new System.Drawing.Point(100, 150);
-            this.txtUserPassword.Name = "txtUserPassword";
-            this.txtUserPassword.Size = new System.Drawing.Size(297, 27);
-            this.txtUserPassword.TabIndex = 13;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(425, 124);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 20);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Email Adresi";
-            // 
-            // txtUserAdress
-            // 
-            this.txtUserAdress.Location = new System.Drawing.Point(523, 150);
-            this.txtUserAdress.Name = "txtUserAdress";
-            this.txtUserAdress.Size = new System.Drawing.Size(297, 27);
-            this.txtUserAdress.TabIndex = 15;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(425, 157);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 20);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Adresi";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(419, 42);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(98, 20);
-            this.label7.TabIndex = 16;
-            this.label7.Text = "Doğum Tarihi";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Erkek",
-            "Kadın"});
-            this.comboBox1.Location = new System.Drawing.Point(523, 81);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(297, 28);
-            this.comboBox1.TabIndex = 17;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(419, 89);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(64, 20);
-            this.label8.TabIndex = 18;
-            this.label8.Text = "Cinsiyeti";
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.Location = new System.Drawing.Point(401, 262);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
-            this.btnUpdate.TabIndex = 20;
-            this.btnUpdate.Text = "Güncelle";
-            this.btnUpdate.UseVisualStyleBackColor = true;
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(560, 262);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(94, 29);
-            this.btnDelete.TabIndex = 21;
-            this.btnDelete.Text = "Sil";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1435, 718);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.txtUserAdress);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.txtUserPassword);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtUserEmail);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtUserLastName);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtUserFirstName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dtpDateOfBirth);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.txtUserName);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Kullanıcı";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
-
-        private Label label1;
-        private TextBox txtUserName;
-        private Button btnAdd;
-        private DataGridView dataGridView1;
-        private DateTimePicker dtpDateOfBirth;
-        private TextBox txtUserFirstName;
-        private Label label2;
-        private TextBox txtUserLastName;
-        private Label label3;
-        private TextBox txtUserEmail;
-        private Label label4;
-        private TextBox txtUserPassword;
-        private Label label5;
-        private TextBox txtUserAdress;
-        private Label label6;
-        private Label label7;
-        private ComboBox comboBox1;
-        private Label label8;
-        private Button btnUpdate;
-        private Button btnDelete;
     }
 }
+
--- WebAPIileFormUygulaması/Form1.cs
+++ WebAPIileFormUygulaması/Form1.cs
@@ -1,195 +1,20 @@
-using Entities.DTOS.UserDTOs;
-using Entities.Enum;
-using System.Net.Http.Json;
-using static System.Net.WebRequestMethods;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WebAPIileFormUygulaması
 {
     public partial class Form1 : Form
     {
-        #region Defines
-        private string Url = "http://localhost:5263/api/";
-        private int selectedID = 0;
-        #endregion
-        #region Form1
         public Form1()
         {
             InitializeComponent();
         }
-         
-
-
-
-        private async void Form1_Load(object sender, EventArgs e)
-        {
-            btnAdd.Enabled = true;
-            btnUpdate.Enabled = false;
-            btnDelete.Enabled = false;
-            await DataGirdViewFill();
-            CmbGenderFill();
-        }
-        #endregion
-        #region Methods
-
-        private void CmbGenderFill()
-        {
-            List<Gender> genders = new List<Gender>();
-            genders.Add(new Gender()
-            {
-                Id = 1,
-                Name="Erkek"
-            });genders.Add(new Gender()
-            {
-                Id = 2,
-                Name="Kadın"
-            });
-            comboBox1.DataSource= genders;
-            comboBox1.DisplayMember= "Name";
-            comboBox1.ValueMember = "Id";
-        }
-        class Gender
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-
-        }
-        private async Task DataGirdViewFill()
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                var users = await client.GetFromJsonAsync<List<UserDetailDto>>(new Uri(Url + "User/GetList"));
-                dataGridView1.DataSource = users;
-            }
-        }
-
-        void ClearForm()
-        {
-            txtUserName.Text = String.Empty;
-            txtUserFirstName.Text = String.Empty;
-            txtUserLastName.Text = String.Empty;
-            txtUserAdress.Text = String.Empty;
-            txtUserPassword.Text = String.Empty;
-            txtUserEmail.Text = String.Empty;
-            dtpDateOfBirth.Value = DateTime.Now;
-            comboBox1.SelectedValue = 0;
-            btnAdd.Enabled = true;
-            btnUpdate.Enabled = false;
-            btnDelete.Enabled = false;
-        }
-
-        #endregion
-
-        #region Crud
-        private async void btnAdd_Click(object sender, EventArgs e)
-        {
-
-            using (HttpClient client = new HttpClient())
-            {
-              
-                
-                UserAddDTO userAddDTO = new UserAddDTO()
-                {
-                    FirstName = txtUserFirstName.Text,
-                    LastName = txtUserLastName.Text,
-                    UserName = txtUserName.Text,
-                    Password = txtUserPassword.Text,
-                    Email = txtUserEmail.Text,
-                    Address = txtUserAdress.Text,
-                    DateOfBirth = Convert.ToDateTime(dtpDateOfBirth.Text),
-                    Gender = comboBox1.Text == "Erkek" ? true : false,
-                };
-                HttpResponseMessage response = await client.PostAsJsonAsync(Url + "User/Add", userAddDTO);
-                if (response.IsSuccessStatusCode)
-                {
-                    MessageBox.Show("Ekleme İşlemi Başarılı");
-                    await DataGirdViewFill();
-                    ClearForm();
-                }
-                else
-                {
-                    MessageBox.Show("Ekleme İşlemi Başarısız");
-                    ClearForm();
-                }
-            }
-        }
-
-   
-
-        private async void dataGridView1_DoubleClick(object sender, EventArgs e)
-        {
-            selectedID = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
-            using (HttpClient client = new HttpClient())
-            {
-                var user = await client.GetFromJsonAsync<UserDto>(Url + "User/GetById/" + selectedID);
-
-                txtUserName.Text = user.UserName;
-                txtUserFirstName.Text = user.FirstName;
-                txtUserLastName.Text = user.LastName;
-                txtUserAdress.Text = user.Address;
-                txtUserPassword.Text = null;
-                txtUserEmail.Text = user.Email;
-                dtpDateOfBirth.Value = user.DateOfBirth;
-                comboBox1.SelectedValue = user.Gender == true ? 1 : 2;   
-            }
-            btnAdd.Enabled = false;
-            btnUpdate.Enabled = true;
-            btnDelete.Enabled = true;
-
-        }
-
-        private async void btnUpdate_Click(object sender, EventArgs e)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-
-
-                UserUpdateDto userUpdateDto = new UserUpdateDto()
-                {
-                    Id = selectedID,
-                    FirstName = txtUserFirstName.Text,
-                    LastName = txtUserLastName.Text,
-                    UserName = txtUserName.Text,
-                    Email = txtUserEmail.Text,
-                    Address = txtUserAdress.Text,
-                    DateOfBirth = Convert.ToDateTime(dtpDateOfBirth.Text),
-                    Gender = comboBox1.Text == "Erkek" ? true : false,
-                    Password= txtUserPassword.Text,
-                };
-                HttpResponseMessage response = await client.PutAsJsonAsync(Url + "User/Update", userUpdateDto);
-                if (response.IsSuccessStatusCode)
-                {
-                    MessageBox.Show("Düzenleme İşlemi Başarılı");
-                    await DataGirdViewFill();
-                    ClearForm();
-                }
-                else
-                {
-                    MessageBox.Show("Düzenleme İşlemi Başarısız");
-                    ClearForm();
-                }
-            }
-
-
-        }
-
-        private async void btnDelete_Click(object sender, EventArgs e)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.DeleteAsync(Url + "User/Delete/" + selectedID);
-                    if (response.IsSuccessStatusCode)
-                     {
-                    MessageBox.Show("Silme İşlemi Başarılı");
-                    await DataGirdViewFill();
-                    ClearForm();
-                     }
-                else
-                {
-                    MessageBox.Show("Silme İşlemi Başarısız");
-                }
-            }
-
-            }
-        #endregion
     }
 }
